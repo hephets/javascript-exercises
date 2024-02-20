91ccda5efff4a6293b05b8b3c652f49e25a815bd
hephets <147001986+hephets@users.noreply.github.com>
2024-02-19 16:43:13 -0800

    Define function repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let count=0;
+  let repeatedString="",
+    while (count < num) {
+      repeatedString += givenString;
+      count++;
+  }
+  return repeatedString
 };
 
 // Do not edit below this line

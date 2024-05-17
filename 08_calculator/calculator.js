e05af4c12d856a3d675cb5633415e4eabbbc72f8
hephets <147001986+hephets@users.noreply.github.com>
2024-05-17 13:13:48 -0700

    First attempt to define all functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a + b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a - b)
 };
 
-const sum = function() {
-	
+const sum = function(arrayX) {
+	let sum = 0;
+	for (i in arrayX) {
+		sum += i;
+	} 
+	return sum
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+	return (a * b)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a ** b)
 };
 
-const factorial = function() {
-	
+const factorial = function(f) {
+	let product = 1
+	for (let i=1; i<=f; i++) {
+		product *= i;
+	} 
+	return product
 };
 
 // Do not edit below this line
